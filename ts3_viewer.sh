5b5e0158435a365dc40236958f8eea45d7fa8bf6
jonybat <jonybat26@hotmail.com>
2018-12-25 14:23:26 +0000

    Use temporary files

--- ts3_viewer.sh
+++ ts3_viewer.sh
@@ -11,15 +11,19 @@ cd "$TSVIEWER_DIR"
 echo "<?php
 session_start();
 \$_SESSION['pubviewer']=true;
-?>" > "$TSVIEWER_FULL"
-curl -k "https://$TSVIEWER_DNS/ts3wi/tsviewpub-full.php?ip=$TSVIEWER_IP&skey=0&sid=3&showicons=right&bgcolor=ffffff&fontcolor=ffffff" >> "$TSVIEWER_FULL"
+?>" > "$TSVIEWER_FULL.tmp"
+curl -k "https://$TSVIEWER_DNS/ts3wi/tsviewpub-full.php?ip=$TSVIEWER_IP&skey=0&sid=3&showicons=right&bgcolor=ffffff&fontcolor=ffffff" >> "$TSVIEWER_FULL.tmp"
+
+mv "$TSVIEWER_FULL.tmp" "$TSVIEWER_FULL"
 
 sleep 5
 
 echo "<?php
 session_start();
 \$_SESSION['pubviewer']=true;
-?>" > "$TSVIEWER_MINI"
-curl -k "https://$TSVIEWER_DNS/ts3wi/tsviewpub-mini.php?ip=$TSVIEWER_IP&skey=0&sid=3&showicons=right&bgcolor=transparent&fontcolor=EEE&linkcolor=DC9E3B&footcolor=AAA&height=200px" >> "$TSVIEWER_MINI"
+?>" > "$TSVIEWER_MINI.tmp"
+curl -k "https://$TSVIEWER_DNS/ts3wi/tsviewpub-mini.php?ip=$TSVIEWER_IP&skey=0&sid=3&showicons=right&bgcolor=transparent&fontcolor=EEE&linkcolor=DC9E3B&footcolor=AAA&height=200px" >> "$TSVIEWER_MINI.tmp"
+
+mv "$TSVIEWER_MINI.tmp" "$TSVIEWER_MINI"
 
 exit 0
